190710751d22e805b7f545bdad4f5474234399c7
MohanaPriyaThiru <mohanapriyat1190@gmail.com>
2026-02-20 13:27:13 +0530

    Update example.spec.js

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -13,7 +13,7 @@ test('get started link', async ({ page }) => {
   console.log("code changed")
 
   // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
+  await page.getByRole('link', { name: 'Get started' }).click();//used locator by role
 
   // Expects page to have a heading with the name of Installation.
   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
